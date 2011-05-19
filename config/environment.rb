f225cc1e17975f682cc5e2f3bac772bbaa7bab77
James Stuart <github@jamesstuart.org>
2011-05-19 11:09:14 -0400

    working with changing credentials

--- config/environment.rb
+++ config/environment.rb
@@ -6,12 +6,13 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 # We need to make sure that rails 2.3.5 is used because that's what Blacklight uses
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
+require "james_monkeys"
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
